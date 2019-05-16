cbff861769b61e459d11af248ec62da66ad8289c
watahak <ngping92@gmail.com>
2019-05-16 17:53:28 +0800

    1 file modified

--- components/git-user.component.ts
+++ components/git-user.component.ts
@@ -8,10 +8,37 @@ import { Markdown } from './markdown.component';
   components: { Markdown },
   template: `
   <div>
-  git user works!
+    <div class = "search">
+      <b-form-input id="input-large" size="lg" v-model="text" placeholder="Enter Username"></b-form-input>
+      <div class="mt-2">Value: {{ text }}</div>
+
+      <b-button variant="outline-primary">
+        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+      </b-button>
+    </div>
+
+    <div class="cardHolder">
+      <b-card title="Card title" sub-title="Card subtitle">
+        <b-card-text>
+          Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+          content.
+        </b-card-text>
+
+        <b-card-text>A second paragraph of text in the card.</b-card-text>
+
+        <a href="#" class="card-link">Card link</a>
+        <b-link href="#" class="card-link">Another link</b-link>
+      </b-card>
+    </div>
+
   <markdown :inlineHtml=myInlineMd></markdown>
   </div>
   `,
+  data() {
+    return {
+      text: ''
+    }
+  }
 })
 export class GitUser extends Vue {
 
@@ -31,15 +58,14 @@ export class GitUser extends Vue {
   created() {
     this.username = 'octocat';
     this.selectedProject = 0;
+
+    this.myInlineMd = "<div>hellow im dummy</div>"
     //this.init();
-
-    // this.myInlineMd = localService.getStubMd();
-
-    localService.postMarkdown(localService.getStubMd()).then(d => {
-      if (d.data) {
-        this.myInlineMd = d.data;
-      }
-    })
+    // localService.postMarkdown(localService.getStubMd()).then(d => {
+    //   if (d.data) {
+    //     this.myInlineMd = d.data;
+    //   }
+    // })
   }
 
   mounted() {
